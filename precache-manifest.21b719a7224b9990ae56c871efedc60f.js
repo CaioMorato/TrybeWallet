5eebcb2284eb739552b400e570cc36298eb21fdc
Caio Morato <morato_dev@protonmail.com>
2021-08-07 02:44:17 -0300

    Updates

---
+++ precache-manifest.21b719a7224b9990ae56c871efedc60f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8382438c005ae10fee23d5b79c59cfc0",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "0462b88ce7557d8c00fb",
+    "url": "/trybewallet/static/css/main.385d921e.chunk.css"
+  },
+  {
+    "revision": "3a77b47a9c4b6b4319c3",
+    "url": "/trybewallet/static/js/2.42518550.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.42518550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0462b88ce7557d8c00fb",
+    "url": "/trybewallet/static/js/main.34a4c1f1.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  },
+  {
+    "revision": "870d242a8f6ba89f9f89cd7072cb6b33",
+    "url": "/trybewallet/static/media/svg.870d242a.svg"
+  }
+]);
