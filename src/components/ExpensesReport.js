45d64fe209cf9c6dad0e14021e0cd4ee731e1dc1
Caio Morato <morato_dev@protonmail.com>
2021-08-07 00:37:03 -0300

    começando estilizar a tabela

--- src/components/ExpensesReport.js
+++ src/components/ExpensesReport.js
@@ -1,20 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import '../styles/ExpensesReport.css';
 
 class ExpensesReport extends React.Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <table className="report-container">
-        <tr>
-          <th>Descrição</th>
-          <th>Categoria</th>
-          <th>Método de Pagamento</th>
-          <th>Valor</th>
-          <th>Taxa do Câmbio</th>
-          <th>Valor Convertido</th>
-          <th>Moeda de conversão</th>
-        </tr>
-      </table>
+      <div className="report-container">
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Categoria</th>
+            <th>Método de Pagamento</th>
+            <th>Valor</th>
+            <th>Taxa do Câmbio</th>
+            <th>Valor Convertido</th>
+            <th>Moeda de conversão</th>
+          </tr>
+          {expenses.map((expense) => (
+            <tr>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{expense.value}</td>
+              <td>{expense.exchangeRates[expense.currency].ask}</td>
+              <td>{expense.exchangeRates[expense.currency].ask * expense.value}</td>
+              <td>{expense.currency}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     );
   }
 }
-export default ExpensesReport;
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(ExpensesReport);
